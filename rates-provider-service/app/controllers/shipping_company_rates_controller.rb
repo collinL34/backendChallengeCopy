010e50d16cc09786c2230e70e6a78f974426f7c8
Collin Shleby <collinlshelby@gmail.com>
2019-08-10 15:14:14 -0700

    implement update functionality for shipping rates model in rails api

--- rates-provider-service/app/controllers/shipping_company_rates_controller.rb
+++ rates-provider-service/app/controllers/shipping_company_rates_controller.rb
@@ -2,6 +2,25 @@ class ShippingCompanyRatesController < ApplicationController
   def get
     shipping_company_rates_data_dump = ShippingCompanyRate.all
 
-    render json: shipping_company_rates_data_dump    
+    render :json => shipping_company_rates_data_dump    
+  end
+
+  def put
+    entry_id = params['id']
+    rate_entry = ShippingCompanyRate.find(entry_id).update(rates_params)
+
+    if rate_entry
+      render :json => rate_entry
+    else
+      render :json => rate_entry.errors.full_messages
+    end
+  end
+
+  private
+
+  def rates_params
+    params.permit(:origin_code,
+                  :destination_code,
+                  :company_id)
   end
 end
--- rates-provider-service/config/routes.rb
+++ rates-provider-service/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/shipping_rates' => 'shipping_company_rates#get'
 
   get '/shipping_companies' => 'shipping_companies#get'
+
+  put '/shipping_rates' => 'shipping_company_rates#put'
 end
